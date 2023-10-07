4d5d39d963ef20ae4e9b324e96c257f64afb5fad
Davi <123986101+Daviqrozz@users.noreply.github.com>
2023-10-07 20:59:25 -0300

    Checkbox adicionado
    Foi adicionado a funçao de alterar a cor atraves de um checkbox

--- scripts.js
+++ scripts.js
@@ -1,16 +1,27 @@
 const html = document.querySelector('html')
 const button = document.querySelector('#button')
-var click = 0
-
+var checkbox = document.getElementById("checkbox");
 button.addEventListener('click', () => {
-   MudarCor()
+    MudarCor()
 })
 
-function MudarCor(){
+checkbox.addEventListener('change',() => {
+    MudarCor()
+})
+
+function MudarCor() {
     const atr = html.getAttribute('data-contexto')
     if (atr == 'white') {
         html.setAttribute('data-contexto', 'black');
-        
+        button.classList.add('active');
+        check.classList.add('text-success')
+        console.log('O site esta escuro')
+
     } else {
         html.setAttribute('data-contexto', 'white');
-}}
+        button.classList.remove('active');
+        check.classList.remove('text-success')
+        console.log('O site esta claro')
+
+    }
+}
